9038bd1493e2d6306f04b5fac888edbeb29fc5f7
aryanjangid <aryanjangid2511@gmail.com>
2022-01-01 13:08:51 +0530

    Backgrond

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Background from './background'
 
 function Home() {
 return (
     <div >
       <h1>home</h1>
+      <Background />
       
     </div>
   );
---
+++ src/Components/Particles.jsx
@@ -0,0 +1,89 @@
+
+import Particles from "react-tsparticles";
+
+function particle() {
+    return (
+<Particles
+        id="tsparticles"
+        options={{
+          background: {
+            color: {
+              value: "00000",
+            },
+          },
+          
+          fpsLimit: 60,
+          interactivity: {
+            detectsOn: "window",
+            events: {
+              onClick: {
+                enable: false,
+                mode: "push",
+                resize:"1",
+              },
+              onHover: {
+                enable: false,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              bubble: {
+                distance: 400,
+                duration: 1,
+                opacity: 0.8,
+                size: 60,
+              },
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#BFC1FF",
+            },
+            links: {
+              color: "#ffffff",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            collisions: {
+              enable: false,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outMode: "bounce",
+              random: false,
+              speed: 6,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                value_area: 800,
+              },
+              value: 80,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              random: true,
+              value: 5,
+            },
+          },
+          detectRetina: true,
+        }}
+      />)}
+export default particle
