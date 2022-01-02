361488a57942ff51793d01ca267f128c8f488871
Sarthak Pawar <sarthakrajesh777@gmail.com>
2022-01-02 16:54:43 +0530

    Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,9 +5,15 @@ const Footer = () => {
     return (
         <>
             <div className="footer">
-                <Link className="footer-link" to='//www.linkedin.com/company/d-y-patil-robotics-ai-club' target="_blank" ><i class="fab fa-linkedin"></i></Link>
-                <Link className="footer-link" to='//www.linkedin.com/company/d-y-patil-robotics-ai-club' target="_blank" ><i class="fab fa-instagram-square"></i></Link>
-                <Link className="footer-link" to='//www.linkedin.com/company/d-y-patil-robotics-ai-club' target="_blank" ><i class="fab fa-twitter-square"></i></Link>
+                <div className="footer-links">
+                    <Link className="footer-link" to='//www.linkedin.com/company/d-y-patil-robotics-ai-club' target="_blank" ><i class="fab fa-linkedin"></i></Link>
+                    <Link className="footer-link" to='//www.linkedin.com/company/d-y-patil-robotics-ai-club' target="_blank" ><i class="fab fa-instagram-square"></i></Link>
+                    <Link className="footer-link" to='//www.linkedin.com/company/d-y-patil-robotics-ai-club' target="_blank" ><i class="fab fa-twitter-square"></i></Link>
+                </div>
+                <div className="copywrite">
+                    <p>@ 2022 D.Y. Patil Robotics & Ai Club</p>
+                    <p>All Rights Reserved.</p>
+                </div>
             </div>
             {/* <div className="background"></div> */}
         </>
