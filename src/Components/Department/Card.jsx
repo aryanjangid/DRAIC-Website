81a882f631a5ca488a7f47280169561c0cf8b0ab
aryanjangid <aryanjangid2511@gmail.com>
2022-01-10 09:38:22 +0530

    Create Card

---
+++ src/Components/Department/Card.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Avatar from "./Avatar";
+import SocialMedia from "./Socialmedia";
+import ProfDepList from "./ProgDepList"
+
+function BasicCard(ProfDepList) {
+    return (
+        <div className="profile-card">
+            <div className="text">
+                <Avatar img={ProfDepList.imgURL} />
+                <h3>{ProfDepList.name}</h3>
+                <p>{ProfDepList.branch}</p>
+                <div className="links">
+                    <SocialMedia linkedin={ProfDepList.linkedinURL} instagram={ProfDepList.instagramURl} />
+                </div>
+            </div>
+        </div>
+    )
+}
+function Card() {
+    return (
+        <div>
+            {ProfDepList.map(BasicCard)}
+        </div>
+
+
+    )
+}
+export default Card
---
+++ src/Components/Department/progdep.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import Card from "./Card";
+function Progdep(){
+    return(<Card />
+    )
+}
+export default Progdep
