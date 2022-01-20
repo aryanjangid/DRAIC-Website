03ff34904993c598632c4605a524832068307487
aryanjangid <89588107+aryanjangid@users.noreply.github.com>
2022-01-20 20:01:05 +0530

    update src

---
+++ src/Components/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Project from './Projects-home';
+
+import Landingslide from './Landingslide';
+import Teams from './Teams';
+
+function Home() {
+  return (
+    <div>
+      <Landingslide />
+      <Project />
+      <Teams />
+    </div>
+  );
+}
+
+export default Home;
+
+
+
---
+++ src/Components/Landingslide.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import draicimage from '../Assets/logo_white.png';
+function Landingslide() {
+    return (
+        <div class="landing-slide">
+
+
+            <h1 className="text">D.Y. Patil Robotics & AI Club<br></br>
+                DYPCOE, Pune</h1>
+            <img className="draic-logo" src={draicimage}></img>
+
+
+        </div>
+
+    )
+}
+export default Landingslide
