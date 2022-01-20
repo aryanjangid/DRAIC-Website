9370d9a7da6cdea6d9675920fc70f7efc8625667
aryanjangid <aryanjangid2511@gmail.com>
2022-01-20 21:32:36 +0530

    url problem resolve

--- src/Project.jsx
+++ src/Project.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import pic1 from "./Assets/project1.jpg"
 import pic2 from "./Assets/project2.jpg"
 import pic3 from "./Assets/project3.jpg"
-import Project from "./Components/Projects-home"
 
 // import "../style.css";
 
