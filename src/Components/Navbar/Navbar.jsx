7c1c3f5fedadd408656fcd777736109d625441a8
Spider9480 <omgawandeofficial9834899149@gmail.com>
2022-02-14 00:37:09 +0530

    Navbar styles added in navbar folder

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
 import "../../App.css"
+import "./Navbar.css"
+// import logo from "../../Assets/logo_white.png"
 
 function Navbar() {
+    
     return (<>
 
         <div classNameName="navbar">
@@ -15,7 +18,17 @@ function Navbar() {
                         <NavLink className="nav-link " activeClassName="active" aria-current="page" to="/project"> <div className="title">Projects</div> <span className='icon'><ion-icon name="rocket"></ion-icon></span> </NavLink>
                     </li>
                     <li className="nav-item">
-                        <NavLink className="nav-link" activeClassName="active" aria-current="page" to="/"> <div className="title">Home</div> <span className='icon'><ion-icon name="home"></ion-icon></span></NavLink>
+                        <NavLink className="nav-link" activeClassName="active" aria-current="page" to="/">
+                            <div className="title">
+                                Home
+                            </div>
+                            <span className='icon hover'>
+                                {/* <img className="logo" src={logo}/> */}
+                                <div className="home">
+                                <ion-icon name="home" ></ion-icon>
+                                </div>
+                            </span>
+                        </NavLink>
                     </li>
                     <li className="nav-item">
                         <NavLink className="nav-link" activeClassName="active" aria-current="page" to="/achivements"><div className="title achievements">Achivements</div><span className='icon'><ion-icon name="medal"></ion-icon></span> </NavLink>
