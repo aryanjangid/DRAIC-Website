b7d6ba13883b8c3d67b3a955c6e8a272251fa3ef
Palash Rambhia <palashrambhia2@gmail.com>
2022-02-16 15:57:25 +0530

    Contact section CSS and responsive done

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -2,80 +2,61 @@ import React from 'react';
 import styles from "./Contact.css"
 
 function Contact() {
-  return (<div>
-    <div className='cmain'>
-      <div className="firstrow">
-        <h1 className="wheredowe">Still, wondering?? Let's talk, and get things done!</h1>
-        <div className="seperator">
-          <div className="linecontactus1"></div>
-          <div>
-            <h1 className="aboutusheading">GET IN TOUCH</h1>
+  return (
+    <div className='contactusfull'>
+      <div className="headingcontainer">
+            <h1 className="get-in-touch">GET IN TOUCH</h1>
+            <hr className="line" /><br></br>
+            <h3 className="send-email-line">
+              Please fill out the form below to send us an email and we will get
+              back to you as soon as possible.
+            </h3>
+      </div>
+      <div className="fullform">
+        <div className="leftside">
+        <form action="https://en.wikipedia.org/wiki/%22Hello,_World!%22_program" className="leftform" method='POST'>
+          <div className="nameandemail">
+            <div className="name">
+              <input type="text" id="fname" name="fname" placeholder="Name" required/>  
+            </div>
+            <div className="emails">   
+              <input type="email" id="lname" name="email" placeholder="Email" required/>
+            </div>   
+          </div> 
+          <div className="textar">
+            <textarea className="message-box" id="subject" name="message" placeholder="Message" required></textarea>
           </div>
-          <div className="linecontactus2"></div>
+          <div className="button">
+            <button className='send-button'>Send</button>
+          </div>
+        </form>
         </div>
-      </div>
-
-
-    </div>
-
-    <div className="contactusouter">
-      <div className="grid-container-contactus">
-        {/* <div className="item1">
-          <h1 className="get-in-touch">GET IN TOUCH</h1>
-          <hr className="line" /><br></br>
-          <h3 className="send-email-line">
-            Still, wondering?? Let's talk, and get things done!
-          </h3>
-        </div> */}
-        <div className="item2">
-
-          <form>
-            <input type="text" id="fname" name="fname" placeholder="Name" />
-          </form>
-        </div>
-        <div className="item3">
-          <form>
-            <input type="text" id="lname" name="lname" placeholder="Email" />
-          </form>
-        </div>
-
-        <div className="item4">
+        <div className="rightside">
           <h1 className="contact-info">Contact Info</h1>
-          <img className="img-t1" alt="email" src="https://img.icons8.com/material-rounded/24/ffffff/new-post.png" />
-          <h1 className="t1">
-            Email
-          </h1><br></br>
-          <h1 className="info-t1">daricdypcoe@gmail.com</h1>
-
-          <img className="img-t1" alt="phone" src="https://img.icons8.com/ios-glyphs/30/ffffff/phone--v1.png" />
-          <h1 className="t1">
-            Phone
-          </h1><br></br>
-          <h1 className="info-t1">+919856324566</h1>
-          <img className="img-t1" alt="email" src="https://img.icons8.com/ios-glyphs/30/ffffff/linkedin.png" />
-          <h1 className="t1">
-            linkedin
-          </h1><br></br>
-          <h1 className="info-t1">daricdypcoe@gmail.com</h1>
+          <div className="email">
+            <img className="img-t1" alt="email" src="https://img.icons8.com/material-rounded/24/ffffff/new-post.png" />
+            <div className="infoemail">
+              <h1 className="t1">Email</h1>
+              <h1 className="info-t1">daricdypcoe@gmail.com</h1>
+            </div>
+          </div>
+          <div className="phone">
+            <img className="img-t1" alt="phone" src="https://img.icons8.com/ios-glyphs/30/ffffff/phone--v1.png" />
+            <div className="infophone">
+              <h1 className="t1">Phone</h1>
+              <h1 className="info-t1">+919856324566</h1>
+            </div>
+          </div>
+          <div className="linkedin">       
+            <img className="img-t1" alt="email" src="https://img.icons8.com/ios-glyphs/30/ffffff/linkedin.png" />
+            <div className="infolink">
+              <h1 className="t1">linkedin</h1>
+              <h1 className="info-t1">daricdypcoe@gmail.com</h1>
+            </div>
+          </div>
         </div>
-
-
-        <div className="item5">
-          <textarea className="message-box" id="subject" name="message" placeholder="Message"></textarea>
-        </div>
-
-
-        <div className="item6">
-          <button className='send-button'>
-            Send
-          </button>
-        </div>
-
       </div>
     </div>
-  </div>
-
-
 
   );
 }
