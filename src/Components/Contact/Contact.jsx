d15e88bc465b41f66707f63e9d42d3c65fb81154
Palash Rambhia <palashrambhia2@gmail.com>
2022-02-19 11:03:26 +0530

    Contact Us Renamed and Small Bugs Fixed

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -3,65 +3,60 @@ import styles from "./Contact.css"
 
 function Contact() {
   return (
-    <div className='contactusouter'>
-
-      <div className="contfirstrow">
-        <h1 className="contwheredowe">OPPORTUNITIES DON’T HAPPEN, YOU CREATE THEM</h1>
-        <div className="seperator">
-          <div className="contline1"></div>
+    <div className='contact_main_body'>
+      <div className="contact_headline_div">
+        <h1 className="contact_headline">OPPORTUNITIES DON'T HAPPEN, YOU CREATE THEM</h1>
+      </div>
+      <div className="seperator">
+          <div className="lineachi1"></div>
           <div>
-            <h1 className="contaboutusheading">GET IN TOUCH</h1>
+            <h1 className="aboutusheading">GET IN TOUCH</h1>
           </div>
-          <div className="contline2"></div>
+          <div className="lineachi2"></div>
         </div>
-
+      <div className="contact_fillform_div">
+            <h3 className="contact_fillform">Please fill out the form below to send us an email and we will get back to you as soon as possible.</h3>
       </div>
-      <div className="headingcontainer">
-            <h3 className="send-email-line">
-              Please fill out the form below to send us an email and we will get
-              back to you as soon as possible.
-            </h3>
-      </div>
-      <div className="fullform">
+      <div className="contact_form_details">
         <div className="leftside">
         <form action="https://en.wikipedia.org/wiki/%22Hello,_World!%22_program" className="leftform" method='POST'>
-          <div className="nameandemail">
-            <div className="name">
+          <div className="contact_name_email_div">
+            <div className="contact_form_name">
               <input type="text" id="fname" name="fname" placeholder="Name" required/>  
             </div>
-            <div className="emails">   
+            <div className="contact_form_email">   
               <input type="email" id="lname" name="email" placeholder="Email" required/>
             </div>   
           </div> 
-          <div className="textar">
-            <textarea className="message-box" id="subject" name="message" placeholder="Message" required></textarea>
+          <div className="contact_form_textarea_div">
+            <textarea className="contact_textarea" id="subject" name="message" placeholder="Message" required></textarea>
           </div>
-          <div className="button">
-            <button className='send-button'>Send</button>
+          <div className="contact_sendbtn_div">
+            <button className='contact_sendbtn'>Send</button>
           </div>
         </form>
         </div>
-        <div className="rightside">
-          <h1 className="contact-info">CONTACT INFORMATION</h1>
-          <div className="email">
-            <img className="img-t1" alt="email" src="https://img.icons8.com/material-rounded/24/ffffff/new-post.png" />
-            <div className="infoemail">
-              <h1 className="t1">Email</h1>
-              <h1 className="info-t1">daricdypcoe@gmail.com</h1>
+        <div className="contact_information_div">
+          <h1 className="contact_info_headline">CONTACT INFORMATION</h1>
+          <div className="contact_info_email_div">
+            <img className="contact_icons" alt="email" src="https://img.icons8.com/material-rounded/24/ffffff/new-post.png" />
+            <div className="contact_infoemail">
+              <h1 className="contact_info_subhead">Email</h1>
+              <h1 className="contact_all_information">daricdypcoe@gmail.com</h1>
             </div>
           </div>
-          <div className="phone">
-            <img className="img-t1" alt="phone" src="https://img.icons8.com/ios-glyphs/30/ffffff/phone--v1.png" />
-            <div className="infophone">
-              <h1 className="t1">Phone</h1>
-              <h1 className="info-t1">+919856324566</h1>
+          <div className="contact_info_phone_div">
+            <img className="contact_icons" alt="phone" src="https://img.icons8.com/ios-glyphs/30/ffffff/phone--v1.png" />
+            <div className="contact_infophone">
+              <h1 className="contact_info_subhead">Phone</h1>
+              <h1 className="contact_all_information">+919856324566</h1>
             </div>
           </div>
-          <div className="linkedin">       
-            <img className="img-t1" alt="email" src="https://img.icons8.com/ios-glyphs/30/ffffff/linkedin.png" />
-            <div className="infolink">
-              <h1 className="t1">linkedin</h1>
-              <h1 className="info-t1">daricdypcoe@gmail.com</h1>
+          <div className="contact_info_linkedin_div">       
+            <img className="contact_icons" alt="linkedin" src="https://img.icons8.com/ios-glyphs/30/ffffff/linkedin.png" />
+            <div className="contact_infolink">
+              <h1 className="contact_info_subhead">linkedin</h1>
+              <h1 className="contact_all_information">daricdypcoe@gmail.com</h1>
             </div>
           </div>
         </div>
