8376cb7aa9a000716985950782503ae3895461b1
Sarthak Pawar <sarthakrajesh777@gmail.com>
2022-03-01 17:30:26 +0530

    contact-us button linked and styled

--- src/Components/Contact2/contact2.jsx
+++ src/Components/Contact2/contact2.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './contact2.css'
 export default function contact2() {
   return (
     <div>
-    <div className='contact_btn_home'>
-
-    </div>
-        {/* <button className='contact_btn_home'>Send</button> */}
+      <Link to='/contact' className='contact-btn-all' >
+        <div className='contact_btn_home' >
+          <div className='contact-us-btn-text' >Contact Us</div>
+        </div>
+      </Link>
+      {/* <button className='contact_btn_home'>Send</button> */}
     </div>
   )
 }
