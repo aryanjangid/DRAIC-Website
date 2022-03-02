03f7496a51a61744e674fa47db8aaf3d447e3634
Spider9480 <omgawandeofficial9834899149@gmail.com>
2022-03-02 19:51:23 +0530

    netlify bugs fixed

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -83,7 +83,7 @@ const Testimonials = () => {
         >
           {testimonials.map((test, i) => (
             <div className="testimonial-card-container">
-              <img src={test.image} />
+              <img src={test.image} alt="loading" />
               <div className="myCarousel">
                 <h3>{test.name}</h3>
                 <h4>{test.position}</h4>
