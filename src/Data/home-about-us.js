882616b994e06ba1e61936762d8aa89586a41456
Sarthak Pawar <sarthakrajesh777@gmail.com>
2022-03-08 15:05:06 +0530

    home slider

--- src/Data/home-about-us.js
+++ src/Data/home-about-us.js
@@ -6,12 +6,12 @@ import ourworkshop from '../../src/Assets/Home About Us Images/college2.jpg'
 const aboutusinfo = [
     {
         id: 1,
-        ourcollegeimage:ourcollege,
-        ourteamimage:ourteam,
-        ourworkshopimage:ourworkshop,
-        whoweare:"A random paragraph generator is a tool provided by various websites that provides you as many random paragraphs as you want. Be it for any work purpose of yours, effortlessly.",
-        whatwedo:"A random paragraph generator is a tool provided by various websites that provides you as many random paragraphs as you want. Be it for any work purpose of yours, effortlessly.",
-        ourmission:"A random paragraph generator is a tool provided by various websites that provides you as many random paragraphs as you want. Be it for any work purpose of yours, effortlessly.",
+        ourcollegeimages: [ourcollege, ourteam, ourworkshop],
+        ourteamimages: [ourteam, ourcollege, ourworkshop],
+        ourworkshopimages: [ourworkshop, ourteam, ourcollege],
+        whoweare: "A random paragraph generator is a tool provided by various websites that provides you as many random paragraphs as you want. Be it for any work purpose of yours, effortlessly.",
+        whatwedo: "A random paragraph generator is a tool provided by various websites that provides you as many random paragraphs as you want. Be it for any work purpose of yours, effortlessly.",
+        ourmission: "A random paragraph generator is a tool provided by various websites that provides you as many random paragraphs as you want. Be it for any work purpose of yours, effortlessly.",
 
     }
 
