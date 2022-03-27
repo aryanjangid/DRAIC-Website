9e1177b55b9f988308f5a0e7d84b94af918692f9
aryanjangid <aryanjangid2511@gmail.com>
2022-03-27 13:49:10 +0530

    project

--- src/Data/home-project-list.js
+++ src/Data/home-project-list.js
@@ -3,18 +3,10 @@ import pic2 from "../Assets/Projects Images/project2.jpg";
 import pic3 from "../Assets/Projects Images/project3.jpg";
 
 const projects = [
-  {
-    id: 1,
-    project_name: "CUSTOMIZED 3 DIMENSIONAL PRINTER",
-    imgURL: pic1,
-    project_content:
-      "Development of 3D Printed Autonomous four-legged robot with Servo Motors for actuation.",
-    githubURL: "https://www.linkedin.com/in/prathisupadhyay/",
-    youtubeURl: "https://www.linkedin.com/in/prathisupadhyay/",
-  },
+  
   {
     id: 2,
-    project_name: "AUTONOMOUS PATH FINDER ROBOT",
+    project_name: "CUSTOMIZED 3 DIMENSIONAL PRINTER",
     imgURL: pic2,
     project_content:
       "Developed a customized 3D Printer, with PLA as a filament by utilizing some open source resources.",
@@ -24,10 +16,19 @@ const projects = [
 
   {
     id: 3,
-    project_name: "CUSTOMIZED 3 DIMENSIONAL PRINTER",
+    project_name: "AUTONOMOUS PATH FINDER ROBOT",
     imgURL: pic3,
     project_content:
       "Made an Autonomous Obstacle avoider and Path finder robot with Omni Wheel drive and Ultrasonic Sensor",
+    githubURL: "https://www.linkedin.com/in/prathisupadhyay/",
+    youtubeURl: "https://www.linkedin.com/in/prathisupadhyay/",
+  },
+  {
+    id: 1,
+    project_name: "AUTONOMOUS QUADRUPLED ROBOT",
+    imgURL: pic1,
+    project_content:
+      "Development of 3D Printed Autonomous four-legged robot with Servo Motors for actuation.",
     githubURL: "https://www.linkedin.com/in/prathisupadhyay/",
     youtubeURl: "https://www.linkedin.com/in/prathisupadhyay/",
   },
