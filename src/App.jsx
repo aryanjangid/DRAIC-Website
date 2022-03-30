a6caaef74f577a5d4cb954891613d76216f23ad1
Spider9480 <omgawandeofficial9834899149@gmail.com>
2022-03-30 22:37:06 +0530

    pr push

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ const App = () => {
 
   const [loading, setLoading] = useState(true);
 
-  setTimeout(() => setLoading(true), 3100);
+  setTimeout(() => setLoading(false), 3100);
 
   return (
     <>
